70f30288ad8f08a0d5c17519eb318bb9b0977a0b
Theresa Sobocinski <tsobocin@ucsd.edu>
2021-01-21 13:07:50 -0800

    new js changes

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -9,10 +9,23 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	console.log("CLICK");
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
 
+$("a.thumbnail").click(projectClick);
+console.log("Click2")
+
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e){
+	console.log("Click3");
+	e.preventDefault();
+	$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+		$("myelement").fadeToggle("active");
 }
